fff059bd9c14c1adff5abfd6f32ff437eaec3390
Bert Van Nuffelen <bert.vannuffelen@gmail.com>
2024-06-17 23:27:31 +0000

    release 3.0.0

--- releases/3.0.0/html/example.js
+++ releases/3.0.0/html/example.js
@@ -8,11 +8,16 @@ function example_structure(exampleid){
 		<div id="` + exampleid + `-tabs-1">
 			<textarea class="validationquery" id="` + exampleid + `-tab1validationquery" name="query" cols="80" rows="16"></textarea>
 			<button class="buttonsample copyturtletoclipboard" id="` + exampleid + `-tabs-1-button-1">Copy</button>
+			<button class="buttonsample openTurtleInConverter" id="` + exampleid + `-tabs-1-button-2">Open in Converter</button>
+	        <button class="buttonsample validateTurtle" id="` + exampleid + `-tabs-1-button-3">Validate</button>
 		</div>
 		<div id="` + exampleid + `-tabs-2">
 			<textarea class="validationquery" id="` + exampleid + `-tab2validationquery" name="query" cols="80" rows="16"></textarea>
 			<button class="buttonsample copyjsonldtoclipboard" id="` + exampleid + `-tabs-2-button-1">Copy</button>
 			<button class="buttonsample openinplayground" id="` + exampleid + `-tabs-2-button-2">Open in Playground</button>
+			<button class="buttonsample openJsonldInConverter" id="` + exampleid + `-tabs-2-button-3">Open in Converter</button>
+	        <button class="buttonsample validateJsonld" id="` + exampleid + `-tabs-2-button-4">Validate</button>
+
 		</div>
 	</div>`;
 	return structure;
@@ -62,6 +67,89 @@ function myIndexOf(list, val) {
     xmlhttp.open("GET", file, true);
     xmlhttp.send();
     return xmlhttp.responseText;
+}
+
+ function loadShape(file, dataGraph, format) {
+    var xmlhttp;
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState === 4 && xmlhttp.status !== 200) {
+            alert('Error when opening the file: ' + file + ' - ' + xmlhttp.status + ' ' + xmlhttp.statusText);
+        } else if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+			newUrl = "https://shacl-playground.zazuko.com/#page=0&shapesGraph=" + encodeURIComponent(xmlhttp.responseText) + "&shapesGraphFormat=text%2Fturtle&dataGraph=" + encodeURIComponent(dataGraph) + "&dataGraphFormat=" + format ;
+			//console.log(newUrl);
+            window.open(newUrl, '_blank');
+        }
+    };
+    xmlhttp.open("GET", file, true);
+    xmlhttp.send();
+    return xmlhttp.responseText;
+}
+
+var dialog = $("<div>", {
+    id: "dialog-form",
+    title: "Validation Result"
+  }).dialog({
+    autoOpen: false,
+    height: 400,
+    width: 700,
+    modal: true,
+	show: { effect: "blind", duration: 400 }
+  });
+
+function validate(model, version, content, format) {
+	request = {
+	"contentToValidate": btoa(content),
+    "contentSyntax": format,
+   "embeddingMethod": "BASE64",
+   "validationType": version,
+   "reportSyntax": "text/turtle"
+	};
+	var itbapi = "https://www.itb.ec.europa.eu/shacl/" + model + "/api/validate" ;
+	$.ajax({
+		type: "POST",
+		url: itbapi,
+		data: JSON.stringify(request),// now data come in this function
+		contentType: "application/json; charset=utf-8",
+		crossDomain: true,
+		dataType: "text",
+		success: function (response, status, jqXHR) {
+			dialog.css("white-space","pre-wrap");
+			dialog.text(response);
+		    dialog.dialog("open");
+			reg = /sh:conforms\s+true/g
+			if(response.search(reg) >=0 )
+				dialog.closest(".ui-dialog").children(".ui-dialog-titlebar").css({"background": "darkgreen", "background-image": "linear-gradient(to bottom,#6a996a,#006400)","color":"white"});
+			else
+				dialog.closest(".ui-dialog").children(".ui-dialog-titlebar").css({"background": "red", "background-image": "linear-gradient(to bottom,#ac6464,#b31c1c)","color":"white"});
+		},
+
+		error: function (jqXHR, exception) {
+		 // error handler
+		var msg = '';
+		if (jqXHR.status === 0) {
+			msg = 'Not connect.\n Verify Network.';
+		} else if (jqXHR.status == 404) {
+			msg = 'Requested page not found. [404]';
+		} else if (jqXHR.status == 500) {
+			msg = 'Internal Server Error [500].';
+		} else if (exception === 'parsererror') {
+			msg = 'Requested JSON parse failed.';
+		} else if (exception === 'timeout') {
+			msg = 'Time out error.';
+		} else if (exception === 'abort') {
+			msg = 'Ajax request aborted.';
+		} else {
+			msg = 'Uncaught Error.\n';
+		}
+		
+		alert('It was not possible to validate, msg:' + msg + ' error:' + jqXHR.responseText);
+		}
+		});
 }
 
 function createTurtleEditorFrom(selector) {
@@ -152,11 +240,44 @@ $(document).ready(function () {
 		var indexValues = $examples.map(function() { return this.id; }) ;
 		var index = myIndexOf(indexValues, exampleid);
 
-		newUrl = "https://json-ld.org/playground/#startTab=tab-expand&json-ld=" + editors[index].CM1.getValue(); 
-		window.open(encodeURI(newUrl), '_blank');
+		newUrl = "https://json-ld.org/playground/#startTab=tab-expand&json-ld=" + encodeURIComponent(editors[index].CM1.getValue()); 
+		window.open(newUrl, '_blank');
 		return false;
 	});
-	
+	$("button.openTurtleInConverter").on('click', function(e) {
+		var exampleid = $(this).parent().parent().attr("exampleid");
+		var indexValues = $examples.map(function() { return this.id; }) ;
+		var index = myIndexOf(indexValues, exampleid);
+
+		newUrl = "https://converter.zazuko.com/#value=" + encodeURIComponent(editors[index].CM0.getValue()) + "&format=text%2Fturtle"; 
+		window.open(newUrl, '_blank');
+		return false;
+	});
+	$("button.openJsonldInConverter").on('click', function(e) {
+		var exampleid = $(this).parent().parent().attr("exampleid");
+		var indexValues = $examples.map(function() { return this.id; }) ;
+		var index = myIndexOf(indexValues, exampleid);
+
+		newUrl = "https://converter.zazuko.com/#value=" + encodeURIComponent(editors[index].CM1.getValue()) + "&format=application%2Fld%2Bjson";
+		window.open(newUrl, '_blank');
+		return false;
+	});
+	$("button.validateTurtle").on('click', function(e) {
+		var exampleid = $(this).parent().parent().attr("exampleid");
+		var indexValues = $examples.map(function() { return this.id; }) ;
+		var index = myIndexOf(indexValues, exampleid);
+		var shapes = validate("dcat-ap", "v2.0", editors[index].CM0.getValue(), "text/turtle");
+		return false;
+	});
+	$("button.validateJsonld").on('click', function(e) {
+		var exampleid = $(this).parent().parent().attr("exampleid");
+		var indexValues = $examples.map(function() { return this.id; }) ;
+		var index = myIndexOf(indexValues, exampleid);
+		var shaclfilepath = "./html/shacl/shapes.ttl" ;
+		//var shapes = loadShape(shaclfilepath, editors[index].CM1.getValue(), "application%2Fld%2Bjson");
+		var shapes = validate("dcat-ap", "v2.0", editors[index].CM1.getValue(), "application/ld+json");
+		return false;
+	});
 	$("div.CodeMirror pre").on('click', function(e) {
 		var et = $(e.target);
 		if(et.hasClass('cm-url'))  {
